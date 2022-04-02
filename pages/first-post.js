5e2f12923cd1778d3ffc1bd54d5aeed538a7c742
Himanshu <himanshumishra500@gmail.com>
2022-04-02 16:14:24 +0530

    changes

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,12 +1,7 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function FirstPost() {
 
-  const myLoader=(url)=>{
-    return url;
-  }
-  const url = "https://cdn.pixabay.com/photo/2016/02/28/12/55/boy-1226964__340.jpg";
   return (
     <>
       <h1>First Post</h1>
@@ -18,13 +13,7 @@ export default function FirstPost() {
           <h1>
             Hello there
           </h1>
-          <Image 
-            loader={()=>myLoader(url)}
-            src={url}
-            width={600}
-            height={400}
-            alt="my image"
-          />
+          
         </main>
       </h2>
     </>
